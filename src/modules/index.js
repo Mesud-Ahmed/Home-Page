3f991716054a0b4e148144d4066b626aa7c4c3ec
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-10-14 16:54:03 +0300

    finish mobile design

--- src/modules/index.js
+++
@@ -1,9 +0,0 @@
-import Ship from "./Ship";
-import { Gameboard } from "./Gameboard";
-import { player } from "./player";
-import { createGrid } from "./dom";
-import { playerShipUi } from "./dom";
-import { GameController } from "./GameController";
-import "../styles.css"
-
-
